339935021605eb52068d4cd0317f76cba93dbb59
Andrey O <ozzy@triasoft.com>
2020-05-20 21:18:20 +0300

    Fix extensions for IEsManager

--- src/MyLab.Elastic/EsManagerExtensions.cs
+++ src/MyLab.Elastic/EsManagerExtensions.cs
@@ -9,13 +9,14 @@ namespace MyLab.Elastic
     /// </summary>
     public static class EsManagerExtensions
     {
-        public static Task IndexAsync<TDoc>(TDoc document)
+        public static Task IndexAsync<TDoc>(this IEsManager mgr, TDoc document)
             where TDoc : class
         {
             return Task.CompletedTask;
         }
 
         public static Task<TDoc> SearchAsync<TDoc>(
+            this IEsManager mgr,
             Func<SearchDescriptor<TDoc>, ISearchRequest> query,
             Func<HighlightDescriptor<TDoc>, IHighlight> highlightSelector = null)
             where TDoc : class
