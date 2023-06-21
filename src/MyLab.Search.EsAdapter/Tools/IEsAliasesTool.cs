41eab5e83c0f774cd8ab423992fa40bf4ff9d33c
Andrey O <ozzy.ext@gmail.com>
2023-06-21 18:19:12 +0300

    Add aliases tool

---
+++ src/MyLab.Search.EsAdapter/Tools/IEsAliasesTool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MyLab.Search.EsAdapter.Inter;
+using Nest;
+
+namespace MyLab.Search.EsAdapter.Tools
+{
+    /// <summary>
+    /// Provides abilities for working with aliases
+    /// </summary>
+    public interface IEsAliasesTool
+    {
+        /// <summary>
+        /// Gets aliases
+        /// </summary>
+        Task<IEnumerable<IEsAliasTool>> GetAliasesAsync(Func<GetAliasDescriptor, IGetAliasRequest> selector = null, CancellationToken cancellationToken = default);
+    }
+
+    class EsAliasesTool : IEsAliasesTool
+    {
+        private readonly IEsClientProvider _clientProvider;
+
+        public EsAliasesTool(IEsClientProvider clientProvider)
+        {
+            _clientProvider = clientProvider ?? throw new ArgumentNullException(nameof(clientProvider));
+        }
+
+        public async Task<IEnumerable<IEsAliasTool>> GetAliasesAsync(Func<GetAliasDescriptor, IGetAliasRequest> selector = null, CancellationToken cancellationToken = default)
+        {
+            var resp = await _clientProvider.Provide().Indices.GetAliasAsync(null, selector, cancellationToken);
+
+            EsException.ThrowIfInvalid(resp);
+
+            return resp.Indices.SelectMany(idx =>
+                idx.Value.Aliases.Select(a =>
+                    new EsAliasTool(a.Key, idx.Key.Name, _clientProvider)
+                )
+            );
+        }
+    }
+}
